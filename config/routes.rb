423813ee8668f11e08e8fbaf351298e0ae7aa08c
Kyle Grondin <kagron12@gmail.com>
2017-10-24 13:30:29 -0500

    modified git ignore

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :foods
   devise_for :users
   resources :inventories
+  resources :pets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 end
