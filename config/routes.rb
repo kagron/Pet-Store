280069cb6124a1b85319549ce1bdfde30a471ac4
Kyle Grondin <kagron12@gmail.com>
2017-10-24 14:40:49 -0500

    Some styles

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :inventories
   resources :pets
+  resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 end
