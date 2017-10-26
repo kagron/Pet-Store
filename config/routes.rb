7d8e010fab4d980ec745d08dd5729f60a7619da2
kagron <kagron12@gmail.com>
2017-10-25 19:28:45 -0500

    Trying some routing changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :stores
-  resources :toys
-  resources :foods
+  resources :stores do
+    resource :pets
+    resource :employees
+    resource :toys
+    resourceZZZz :foods
+  end
+
   devise_for :users
   resources :inventories
-  resources :pets
-  resources :employees
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 end
